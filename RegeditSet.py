751ebca1acd7413bd712fe584c327215210d7f42
TheOriginalVoid <jamesbpham1@gmail.com>
2025-11-14 13:14:15 -0800

    Update RegeditSet.py

--- RegeditSet.py
+++ RegeditSet.py
@@ -4,16 +4,16 @@ import winreg
 path = winreg.HKEY_LOCAL_MACHINE
 
 defender = winreg.OpenKeyEx(path, r"SOFTWARE\\Policies\\Microsoft\\Windows Defender\\")
-
-winreg.SetValueEx(defender, 0, winreg.REG_DWORD, 1)
-winreg.SetValueEx(defender + r"DisableAntiSpyware\\", 0, winreg.REG_DWORD, 0)
-winreg.SetValueEx(defender + r"DisableAntiVirus\\", 0, winreg.REG_DWORD, 0)
-winreg.SetValueEx(defender + r"PUAProtection\\", 0, winreg.REG_DWORD, 1)
+try:
+  winreg.DeleteValue(defender,r"DisableAntiSpyware\\")
+  winreg.DeleteValue(defender,r"DisableAntiVirus\\")
+  winreg.SetValueEx(defender,r"PUAProtection\\", 0, winreg.REG_DWORD, 1)
 #Defender Registry Policy
 
 policy = winreg.OpenKeyEx(path, r"SOFTWARE\\Microsoft\Windows\\CurrentVersion\\Policies\\System\\")
 
-winreg.SetValueEx(policy+r"TypeofAdminApprovalMode\\", 0, winreg.REG_DWORD, 2)
+winreg.SetValueEx(policy,r"TypeofAdminApprovalMode\\", 0, winreg.REG_DWORD, 2)
+
 
 
 print("Success!")
